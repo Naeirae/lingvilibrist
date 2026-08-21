f808935548e28aaf0589a7de1b9daf8954cce051
Naeirae <nekurismarieykjuri@gmail.com>
2026-08-21 23:06:35 +0300

    Expose public core API

---
+++ packages/core/src/index.js
@@ -0,0 +1,9 @@
+export { analyzeText } from './analyze.js';
+export {
+  FINDING_KINDS,
+  FINDING_SEVERITIES,
+  FindingValidationError,
+  makeFinding,
+  validateFinding
+} from './finding.js';
+export { dedupeKey, normalizeFindings } from './normalize.js';
